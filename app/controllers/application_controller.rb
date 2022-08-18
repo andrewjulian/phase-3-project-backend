fd73b7bbee39f27f621db2dbee082e50ca5117d0
+ <+>
2022-08-17 18:00:14 -0600

    First Attempt at Application Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,44 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    students = Students.all.order(:name)
+    students.to_json
+  end
 
-    
+  post '/students' do
+    newStudent = Student.create(
+      name: params[:name],
+      grade_level: params[:grade_level]
+    )
+    newStudent.to_json
+  end
+
+  post '/assignments' do
+    Student.all.each do |student|
+      newAssignment = Assignment.create(
+        student_id: student.id, 
+        title: params[:title], 
+        category: params[:category],
+        description: params[:description], 
+        possible_points: params[:possible_points], 
+        due_date: Date.parse(params[:due_date])
+      )
+      newAssignment.to_json
+    end
+  end
+
+  delete '/assignment/:id' do
+    deleteAssignment = Assignment.find(params[:id])
+    deleteAssignment.destroy
+    deleteAssignment.to_json
+  end
+
+  patch '/student/assignment/:id' do
+    updateScore = Assignment.find(params[:id])
+    updateScore.update(
+      earned_points: params[:earned_points],
+    )
+    updateScore.to_json
   end
 
 end
