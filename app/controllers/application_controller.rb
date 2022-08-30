836d2de93e51b636e4b07751a302b687426e2c2d
+ <+>
2022-08-30 14:47:11 -0600

    added new assignment post controller and manual delete controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,17 @@ class ApplicationController < Sinatra::Base
     assignments.to_json(include: :student)
   end
 
-  post '/assignments' do
+  post '/new-assignment' do
     newAssignment = Assignment.create(
-      student_id: student.id, 
+      student_id: params[:name], 
       title: params[:title], 
       category: params[:category],
       description: params[:description], 
       possible_points: params[:possible_points], 
+      earned_points: params[:earned_points],
       due_date: Date.parse(params[:due_date])
     )
-    newAssignment.to_json
+    newAssignment.to_json(include: :student)
   end
 
   post '/new-student' do
@@ -33,6 +34,12 @@ class ApplicationController < Sinatra::Base
 
   delete '/assignments/:id' do
     deleteAssignment = Assignment.find(params[:id])
+    deleteAssignment.destroy
+    deleteAssignment.to_json
+  end
+
+  delete '/assignments' do
+    deleteAssignment = Assignment.find(11)
     deleteAssignment.destroy
     deleteAssignment.to_json
   end
